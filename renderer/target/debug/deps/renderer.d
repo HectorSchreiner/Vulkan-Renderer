42bc470312550523c41a8fd90184cf54d4ae8cc8
Hector <agenthector007@gmail.com>
2025-03-15 16:27:09 +0100

    init

---
+++ renderer/target/debug/deps/renderer.d
@@ -0,0 +1,5 @@
+D:\Programming\Rust\Vulkan-Renderer\renderer\target\debug\deps\renderer.exe: src/main.rs
+
+D:\Programming\Rust\Vulkan-Renderer\renderer\target\debug\deps\renderer.d: src/main.rs
+
+src/main.rs:
---
+++ renderer/target/debug/renderer.d
@@ -0,0 +1,1 @@
+D:\Programming\Rust\Vulkan-Renderer\renderer\target\debug\renderer.exe: D:\Programming\Rust\Vulkan-Renderer\renderer\src\main.rs
